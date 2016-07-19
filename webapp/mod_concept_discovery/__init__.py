cdddfd725dde0657a4fd6164a03d6eba5ab6bd12
unknown <SimonCB765@gmail.com>
2016-07-19 10:32:31 +0100

    Clarified comment

--- webapp/mod_concept_discovery/__init__.py
+++ webapp/mod_concept_discovery/__init__.py
@@ -7,5 +7,5 @@ from flask import Blueprint
 # Create the blueprint for this module.
 modConceptDiscovery = Blueprint('conceptDiscovery', __name__, url_prefix='/concept_discovery')
 
-# Import after defining modCore, as urls needs to import modConceptDiscovery.
+# Import after defining modConceptDiscovery, as urls needs to import modConceptDiscovery.
 from . import urls
